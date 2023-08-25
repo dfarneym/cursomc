ae796d7ff08d8cb11b33137e6a64e39ba6dca3b0
Farney <f_farney@hotmail.com>
2023-08-25 15:07:49 -0300

    Banco de dados H2 e a criação automatica da base de dados

--- src/main/java/com/farney/cursomcj/domain/Categoria.java
+++ src/main/java/com/farney/cursomcj/domain/Categoria.java
@@ -3,15 +3,23 @@ package com.farney.cursomcj.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 /*
  * Serializable é uma interface 
  * que transforma a classe pode converter 
  * seus itens em uma sequencia de bytes
  * Isso serve para os objetos possom ser gravados em arquivos, trafegar em rede
 */
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)/*Garantindo a geração de Id's automaticos*/
 	private Integer id;
 	private String nome;
 	
